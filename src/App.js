9d148e742ec1ca451f60099866a43aacbe083fd2
Unknown <a.vesnenok@student.fontys.nl>
2017-10-23 13:12:09 +0200

    JASON file addition

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 const JsonObj = {
-  Questions: []
+  ItemList: [
+      "Tell your most embarrassing story",
+      "What is your biggest fear?",
+      "Tell about your family",
+      "Do you think the present is better than 50 years ago? Why?",
+      "If you could have one hour talk with any person in the world, who would it be?",
+      "What is the one thing about yourself that you would like me to know?",
+      "What is the thing you don’t like about yourself?",
+      "What is the thing you like about yourself the most?",
+      "Take three minutes and tell the story of your life in as much detail as possible?",
+      "If you could wake up tomorrow having gained any one ability or superpower, what would it be and why?",
+      "For what in your life do you feel most grateful?",
+      "What are you most proud about?",
+      "Which is your favorite book?",
+  ]
 }
 
 class App extends Component {
@@ -12,21 +26,36 @@ class App extends Component {
     super(props);
 
     this.state = {
-      NewQuestions: [],
-      OldQuestions: [],
+      AllQuestions: JsonObj.ItemList,
       currentQuestion: undefined
     }
   }
 
   componentDidMount() {
-    console.debug("i Mounted! im a big boy");
+    console.debug("i Mounted! im a big boy", JsonObj);
+  }
+
+  getRandomQuestion() {
+    const { AllQuestions } = this.state;
+    const rand = Math.floor((Math.random() * AllQuestions.length));
+
+    if(AllQuestions[rand] != null) {
+      this.setState({
+        AllQuestions: AllQuestions
+      });
+      const newQuestion = AllQuestions[rand];
+      if (rand > -1) {
+          AllQuestions.splice(rand, 1);
+      }
+      return newQuestion;
+    } else {
+      return 'Good job! Now you got to know each other better :)';
+    }
   }
 
   onButtonClick = () => {
-    const questions = [];
-
     this.setState({
-      currentQuestion: 'Hi guys test question!'
+      currentQuestion: this.getRandomQuestion()
     })
   }
 
